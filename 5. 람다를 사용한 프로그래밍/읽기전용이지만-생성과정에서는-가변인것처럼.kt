abfa3676eecdd850c989166a3a17efdf8e3c5c90
mingdodev <mingdo.dev@gmail.com>
2025-09-25 18:03:32 +0900

    20250925

---
+++ 5. 람다를 사용한 프로그래밍/읽기전용이지만-생성과정에서는-가변인것처럼.kt
@@ -0,0 +1,18 @@
+val fibonacci = buildList {
+    addAll(listof (1, 1, 2))
+    add (3)
+    add index = 0, element = 3)
+    }
+
+val shouldAdd = true
+
+val fruits = buildSet {
+    add ("Apple")
+    if (shouldAdd) {
+        addA11(listof ("Apple", "Banana", "Cherry"))
+    }
+}
+val medals = buildMap<String, Int> {
+    put ("Gold", 1)
+    putAll(listof("Silver" to 2, "Bronze" to 3))
+}
